8d58494df181464f93683e9aaf3444e8fc28dec6
Minjun Kang <minjun@kaaang.dev>
2022-12-08 03:10:17 +0900

    feat: AWS instance 생성을 추가하라

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ import loading from 'loading-cli';
 
 import fs from 'fs';
 
-import { generateDockerFileFromArray } from 'dockerfile-generator/lib/dockerGenerator.js';
-import { Docker } from 'node-docker-api';
+import {generateDockerFileFromArray} from 'dockerfile-generator/lib/dockerGenerator.js';
+import {Docker} from 'node-docker-api';
 import tar from 'tar-fs';
 
+import AWS from 'aws-sdk';
+import { NodeSSH } from 'node-ssh';
+
 import path from 'path';
-import { fileURLToPath } from 'url';
+import {fileURLToPath} from 'url';
 
 const _TEMPLATES = ['NodeJSWebApp', 'StaticWeb', 'PHP7'];
 
@@ -26,21 +29,35 @@ const StartDeploy = async () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
 
+  const configPath = path.join('.onedep.json');
+  const configExists = fs.existsSync(configPath);
+  let config = configExists ? JSON.parse(fs.readFileSync(configPath, 'utf8')) : null;
+
   const templates = Object.fromEntries(_TEMPLATES.map((template) => [template, JSON.parse(fs.readFileSync(path.join(__dirname, 'templates', `${template}.json`)).toString())]));
 
-  const response = await inquirer.prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: '배포할 프로젝트 명을 입력하세요.',
-    },
-    {
-      type: 'list',
-      name: 'platform',
-      message: '배포할 코드의 플랫폼을 선택하세요.',
-      choices: Object.values(templates).map((template) => template.name),
-    },
-  ]);
+  let response;
+  if (config === null) {
+    response = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'email',
+        message: '이메일 주소를 입력해 주세요.',
+      },
+      {
+        type: 'input',
+        name: 'name',
+        message: '배포할 프로젝트 명을 입력하세요.',
+      },
+      {
+        type: 'list',
+        name: 'platform',
+        message: '배포할 코드의 플랫폼을 선택하세요.',
+        choices: Object.values(templates).map((template) => template.name),
+      },
+    ]);
+  } else {
+    response = config;
+  }
 
   const selectedPlatform = Object.entries(templates).find((template) => template[1].name === response.platform)[1];
 
@@ -48,7 +65,7 @@ const StartDeploy = async () => {
     ...response,
   };
 
-  if (selectedPlatform.custom_args) {
+  if (config === null && selectedPlatform.custom_args) {
     const customArgs = await inquirer.prompt(Object.entries(selectedPlatform.custom_args).map(([argName, message]) => ({
       type: 'input',
       name: argName,
@@ -61,13 +78,96 @@ const StartDeploy = async () => {
     }
   }
 
+  if (config === null) {
+    let load = loading("새로운 EC2 Instance를 생성하는 중...");
+    load.start();
+
+    AWS.config.update({ region: 'ap-northeast-2' });
+    // Create EC2
+    const ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
+
+    // Create Key Pair
+    const keyPair = await ec2.createKeyPair({
+      KeyName: `OneDep_${responses.name}`,
+    }).promise();
+
+    // Create Security Group
+    const vpcs = await ec2.describeVpcs().promise();
+    const vpc = vpcs.Vpcs[0];
+
+    const securityGroup = await ec2.createSecurityGroup({
+      Description: `OneDep_${responses.name}`,
+      GroupName: `OneDep_${responses.name}`,
+      VpcId: vpc.VpcId,
+    }).promise();
+
+    await ec2.authorizeSecurityGroupIngress({
+      GroupId: securityGroup.GroupId,
+      IpPermissions: [
+        {
+          IpProtocol: "tcp",
+          FromPort: 1,
+          ToPort: 65535,
+          IpRanges: [{"CidrIp":"0.0.0.0/0"}]
+        },
+      ]
+    }).promise();
+
+    // Create EC2 Instance
+    const instanceParams = {
+      ImageId: 'ami-0eddbd81024d3fbdd',
+      InstanceType: 't2.micro',
+      KeyName: keyPair.KeyName,
+      MinCount: 1,
+      MaxCount: 1,
+      SecurityGroupIds: [securityGroup.GroupId],
+    };
+
+    const instancePromise = await new AWS.EC2({apiVersion: '2016-11-15'}).runInstances(instanceParams).promise();
+    const instanceId = instancePromise.Instances[0].InstanceId;
+
+    // Wait for the instance to be running
+    await ec2.waitFor('instanceRunning', {InstanceIds: [instanceId]}).promise();
+
+    // Get IPv4
+    const instance = await ec2.describeInstances({InstanceIds: [instanceId]}).promise();
+    const ipv4 = instance.Reservations[0].Instances[0].PublicIpAddress;
+
+    config = { ...responses };
+    config.instanceId = instanceId;
+    config.instanceIpv4 = ipv4;
+    config.instancePrivKey = keyPair.KeyMaterial;
+    config.registryPassword = `${Math.random().toString(36).slice(-8)}${Math.random().toString(36).slice(-8)}${Math.random().toString(36).slice(-8)}`;
+
+    const ssh = new NodeSSH();
+    await ssh.connect({
+      host: ipv4,
+      username: 'ec2-user',
+      privateKey: keyPair.KeyMaterial,
+    });
+
+    const commands = fs.readFileSync(path.join(__dirname, 'commands', 'Docker')).toString().replace(/\${(\w+)}/g, (match, argName) => config[argName]).replace(/\r/g, '');
+    const shell = await ssh.requestShell();
+
+    fs.writeFileSync(configPath, JSON.stringify(config, null, 2));
+
+    await (() => new Promise((resolve) => {
+      shell.on('close', () => {
+        resolve();
+      });
+      shell.end(commands);
+    }))();
+
+    load.succeed(`EC2 생성 완료 (${instanceId})`);
+  }
+
   const dockerfile = ReplaceDockerfileArgs(selectedPlatform.Dockerfile, responses);
   const generatedDockerfile = generateDockerFileFromArray(dockerfile);
 
   const docker = new Docker();
 
   const pack = tar.pack(process.cwd());
-  pack.entry({ name: 'Dockerfile' }, generatedDockerfile);
+  pack.entry({name: 'Dockerfile'}, generatedDockerfile);
 
   const load = loading("Docker Image를 빌드하는 중...");
   load.start();
